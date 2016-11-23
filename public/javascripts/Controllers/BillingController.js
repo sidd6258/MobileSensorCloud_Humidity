276b5c52512514ca5842a28313ec1d30d8044901
siddharthgupta <siddharth6258@gmail.com>
2016-11-22 21:45:16 -0800

    fixing dashboard

--- public/javascripts/Controllers/BillingController.js
+++ public/javascripts/Controllers/BillingController.js
@@ -5,5 +5,8 @@ mbsHumidity.controller("BillingController", function($scope,$http,$location){
 	$scope.billing = function() {
 		$location.path("/billing");				
 	};
-			
+	$scope.dashboard=function(){
+		console.log("dashboard from sensor");
+		$location.path("/dashboard");
+	}	
 });
