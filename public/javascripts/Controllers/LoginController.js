774477458880e53d22a89f4b89ed1bbcb2f9d1f2
ajay <ajaymodi12@gmail.com>
2016-11-23 20:46:07 -0800

    sql

--- public/javascripts/Controllers/LoginController.js
+++ public/javascripts/Controllers/LoginController.js
@@ -1,4 +1,4 @@
-mbsHumidity.controller("LoginController", function($scope,$http,$location){
+mbsHumidity.controller("LoginController", function($scope,$http,$location,$window){
       $scope.SignIn=function(){
   	  console.log("in12");
   	  console.log("pass"+$scope.password);
@@ -15,6 +15,10 @@ mbsHumidity.controller("LoginController", function($scope,$http,$location){
   			}
        		else
        			{
+       			$window.sessionStorage.userid=data[0]["id"];
+     			$window.sessionStorage.username=data[0]["email"];
+     			$window.sessionStorage.firstname=data[0]["firstname"];
+     			$window.sessionStorage.lastname=data[0]["lastname"];
        			$location.path("/dashboard");
        			}
        }).error(function (data, status, headers, config) {
--- routes/home.js
+++ routes/home.js
@@ -2,19 +2,39 @@
  * http://usejsdoc.org/
  */
 var ejs = require("ejs");
+var mysql = require('./mysql');
 
 exports.afterSignIn= function afterSignIn(req,res){
 	// check user already exists
 	console.log(req.param);
 	var password= req.param("password");
 	var email= req.param("email");
-	if(password = "123" &&  email == "siddharth6258@gmail.com"){
-				console.log("valid Login");
-				res.send("valid Login");
+	var getUser="select * from USER_DETAILS where email='"+req.param("email")+"' and password='" + password +"'";
+	console.log("Query is:"+getUser);
+	var flag=0;
+	mysql.fetchData(function(err,results){
+		if(err){
+			throw err;
+		}
+		else 
+		{
+			if(results.length > 0){
+				console.log(req.session);
+				req.session.username=results[0]["email"];
+     			req.session.password=results[0]["password"];
+     			req.session.userid=results[0]['id'];
+     			req.session.name=results[0]["firstname"]+results[0]["lastname"];
+     			console.log(req.session.username);
+				res.send(results);
 			}
 			else {    
+				
+				console.log("Invalid Login");
 				res.send("Invalid Login");
+			 flag=1;
 			}
+		}  
+	},getUser);	
 };
 exports.afterSignUp= function afterSignUp(req,res){
 	var firstname= req.param("firstname");
